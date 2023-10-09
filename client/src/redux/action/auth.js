ccb5b5a5759acffbcc82783ee23a4e273a5d88cb
Akib3822 <akibmujawar44@gmail.com>
2023-10-09 10:31:24 +0530

    Entry Form Functionality

--- client/src/redux/action/auth.js
+++ client/src/redux/action/auth.js
@@ -2,10 +2,10 @@ import { USER_DETAILS_UPDATED } from "../constant/constant";
 import {store} from "../store/store"
 import { bindActionCreators } from "redux";
 
-const userDetails = (username, password,userType)=>{
+const userDetails = (userDetails)=>{
     return({
         type:USER_DETAILS_UPDATED,
-        username,password,userType
+        userDetails
     })
 }
 
